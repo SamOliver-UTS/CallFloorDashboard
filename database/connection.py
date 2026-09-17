c4b4e63d2d35abbb738d2add163ff22284a3c64f
SamOliver-UTS <sam.oliver@unitedteleservice.com>
2026-09-17 15:09:06 +0800

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -2,8 +2,14 @@ import pyodbc
 import pandas as pd
 
 def get_connection():
-    return pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER=KWPCICDBA002;DATABASE=DataAnalysis;Trusted_Connection=yes;")
+    return pyodbc.connect(
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        "SERVER=KWPCICDBA002;"
+        "DATABASE=DataAnalysis;"
+        "Trusted_Connection=yes;"
+        "TrustServerCertificate=yes;"
+    )
 
 def load_callfloor():
-    sql="select * from dws.vw_CallFloorOverview"
-    return pd.read_sql(sql,get_connection())
+    sql = "SELECT * FROM dws.vw_CallFloorOverview"
+    return pd.read_sql(sql, get_connection())
